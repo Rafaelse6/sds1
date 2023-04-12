cb847d17dd5e82cd5841a815e085c2476d3063de
Rafael Santos <rafa.05125@gmail.com>
2023-04-12 20:02:26 -0300

    GET records

--- backend/src/main/java/com/rafaelsantos/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/rafaelsantos/dspesquisa/repositories/RecordRepository.java
@@ -1,9 +1,18 @@
 package com.rafaelsantos.dspesquisa.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.Instant;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.rafaelsantos.dspesquisa.entities.Record;
 
-public interface RecordRepository extends JpaRepository<Record, Long>{
 
+public interface RecordRepository extends JpaRepository<Record, Long> {
+
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 }
